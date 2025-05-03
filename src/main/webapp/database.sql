96dda0ab88bd54dbbb4ffce673007dde7f850f71
BhagyaZ <bhagyaweerasooriya734@gmail.com>
2025-05-03 13:07:50 +0530

    database created

---
+++ src/main/webapp/database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `bomb`.`products` (
+                                   `product_id` INT NOT NULL AUTO_INCREMENT,
+                                   `name` VARCHAR(100) NULL,
+                                   `category` VARCHAR(100) NULL,
+                                   `price` VARCHAR(100) NULL,
+                                   `quantity` VARCHAR(100) NULL,
+                                   `description` VARCHAR(255) NULL,
+                                   PRIMARY KEY (`product_id`));

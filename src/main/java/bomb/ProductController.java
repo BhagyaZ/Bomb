c81c6ffa5cfdbbea9856638bef7744ff019e3b88
BhagyaZ <bhagyaweerasooriya734@gmail.com>
2025-05-05 09:09:06 +0530

    product controller

--- src/main/java/bomb/ProductController.java
+++ src/main/java/bomb/ProductController.java
@@ -1,18 +1,35 @@
-//package bomb;
-//
-//import java.sql.Connection;
-//import java.sql.ResultSet;
-//import java.sql.Statement;
-//
-//public class ProductController {
-//
-//    private static boolean isSuccess;
-//    private static Connection con=null;
-//    private static Statement stmt=null;
-//    private static ResultSet rs=null;
-//
-//    private static boolean insertdata(String name,String category,String price,String quantity,String description){
-//
-//    }
-//
-//}
+package bomb;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ProductController {
+
+    private static boolean isSuccess;
+    private static Connection con=null;
+    private static Statement stmt=null;
+    private static ResultSet rs=null;
+
+    private static boolean insertdata(String name,String category,String price,String quantity,String description){
+        boolean isSuccess = false;
+        try{
+            con=DBConnection.getConnection();
+            stmt=con.createStatement();
+
+            String sql="inseert into book values(0,'"+name+"','"+category+"','"+price+"','"+quantity+"','"+description+"')";
+            int rs=stmt.executeUpdate(sql);
+            if(rs>0){
+                isSuccess=true;
+            }
+            else{
+                isSuccess=false;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return isSuccess;
+
+    }
+
+}

05b9bfc21633e187855792c41655a43feca6d6f2
BhagyaZ <bhagyaweerasooriya734@gmail.com>
2025-05-07 18:23:35 +0530

    insert product

--- src/main/java/bomb/InsertProductServlet.java
+++ src/main/java/bomb/InsertProductServlet.java
@@ -3,31 +3,34 @@ package bomb;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-import java.io.IOException;
 
-@WebServlet("/InsertProductServlet")
+import java.io.IOException;
+ @WebServlet("/InsertProductServlet")
 public class InsertProductServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name=request.getParameter("name");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String name = request.getParameter("name");
         String category = request.getParameter("category");
-        String price=request.getParameter("price");
-        String quantity=request.getParameter("quantity");
-        String description=request.getParameter("description");
+        String price = request.getParameter("price");
+        String quantity = request.getParameter("quantity");
+        String description = request.getParameter("description");
 
-        boolean isTrue;
-        isTrue=ProductController.insertdata(name,category,price,quantity,description);
+        System.out.println(name);
+        System.out.println(category);
+        System.out.println(price);
+        System.out.println(quantity);
 
-        if(isTrue==true){
-            String alertMessage="Data inserted successfully";
-            response.getWriter().println("<script> alert('"+alertMessage+"');window.location.href='addproductform.jsp';</script>");
+        boolean isTrue = ProductController.insertdata(name, category, price, quantity, description);
 
-        }
-        else{
-            RequestDispatcher dis2=request.getRequestDispatcher("wrong.jsp");
+        if (isTrue) {
+            String alertMessage = "Data inserted successfully";
+            response.getWriter().println("<script>alert('" + alertMessage + "');window.location.href='addproductform.jsp';</script>");
+        } else {
+            RequestDispatcher dis2 = request.getRequestDispatcher("wrong.jsp");
             dis2.forward(request, response);
         }
     }
 }
-
